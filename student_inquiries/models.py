7d8f1948a1c480db3ec01daf776c1bf5a2af1726
MUSHIMIYE Patrick <p.mushimiy@alustudent.com>
2023-10-07 15:48:10 +0200

    perf: new field added in the model

--- student_inquiries/models.py
+++ student_inquiries/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
-# Create your models here.
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField(max_length=4000, unique=True)
+    created_at = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
-        return self.message
+        return self.user.username
